f0e90526bf9129ecf2cfc5c5d0e7219626eaef75
JAVA_Tutorial <ahmad2020jafari@gmail.com>
2026-02-19 20:53:16 +0100

    IF Conditional statement and implementing the && Operators

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,6 +1,20 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.print("Hello, Tim");
+        boolean isAlien = false;
+        if (isAlien == false) {
+            System.out.println("It is not an Alien!");
+            System.out.println("And i am scared of Aliens");
+        }
+        int topScore = 80;
+        if (topScore < 100){
+            System.out.println("Yot got the high score!");
+        }
+        int secondTopScore = 60;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Greater than second top score und less than 100 !");
+
+        }
+
     }
 }
