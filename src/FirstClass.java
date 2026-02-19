57dd5393c5fe0fef2c8cf23b8f40ba69811f831e
JAVA_Tutorial <ahmad2020jafari@gmail.com>
2026-02-19 21:18:52 +0100

    Using the OR Operator and ! NOT in if Statement

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -11,10 +11,15 @@ public class FirstClass {
             System.out.println("Yot got the high score!");
         }
         int secondTopScore = 60;
-        if (topScore > secondTopScore && topScore < 100) {
+        if ((topScore > secondTopScore && topScore < 100)) {
             System.out.println("Greater than second top score und less than 100 !");
-
         }
-
+        if (topScore > 90 || secondTopScore <= 90) {
+            System.out.println("Either or both of the conditions are true");
+        }
+        boolean isCar = false;
+        if (!isCar) {
+            System.out.println("This is not suppose to happen!");
+        }
     }
 }
