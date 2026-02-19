aa301e1d6ac7084a5a962e28f59b202ce604ea31
JAVA_Tutorial <ahmad2020jafari@gmail.com>
2026-02-19 22:38:07 +0100

    Done the challenge

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -34,5 +34,27 @@ public class FirstClass {
         String ageText = ageOfClient >= 18 ? "Over Eighten" : "Still Kid";
         System.out.println(ageText);
 
+        // Challenge
+        // 1. create a double variable with a value of 20.00.
+        // 2. create a second variable of type double with a value of 80.00.
+        // 3. add both numbers together, then multiply by 100.00.
+        // 4. use the remainder Operator, to figure out what the remainder from the result of the operation in step three, and 40.00 will be.
+        // 5. create a boolean variable that assign the value true, if the remainder in step four is 0.00, or false if it is not zero.
+        // 6. output the boolean variable just to see what the result is.
+        // 7. write an if then statement that displays a message, "got some remainder"if the boolean in step five is not true.
+
+        double firstNumber = 20.00d;
+        double secondNumber = 80.00d;
+        double total  = 100 * (firstNumber + secondNumber);
+        System.out.println("My Total is = " + total);
+        double theRemainder = total % 40.00d;
+        System.out.println("theRemainder is = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0 ) ? true : false;
+        System.out.println("IsNoRemainder is = " + isNoRemainder);
+        if (!isNoRemainder) {
+            System.out.println("Got some Remainder");
+        }
+
+
     }
 }
