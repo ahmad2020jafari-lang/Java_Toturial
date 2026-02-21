80c10f73d228f0e1c636b423f30fa60d3dd61d4f
JAVA_Tutorial <ahmad2020jafari@gmail.com>
2026-02-22 00:13:24 +0100

    Create the CalulateScore Method to reuse the code

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,15 @@
 public class Main {
     public static void main(String[] args){
-        boolean gameOver = true;
-        int score =  10000;
-        int levelCompleted = 8;
-        int bonus = 200;
+        calculateScore(true, 800,  5, 100);
+        calculateScore(true, 10000, 8, 200);
+    }
+    // Making a Method to reuse the code and avoid the repeat.
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus){
         int finalScore = score;
-
         if (gameOver) {
             finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score was " + finalScore);
-
-            // Challenge
-            // Score = 10000
-            // levelCompleted 8
-            // bonus 200
-        score = 10000;
-        levelCompleted = 8;
-        bonus = 200;
-        finalScore = score;
-            if (gameOver) {
-                finalScore += (levelCompleted * bonus);
-                finalScore += 1000;
-                System.out.println("Your final score was : " + finalScore);
-            }
-
+            finalScore += 1000;
+            System.out.println("Your final score was : " + finalScore);
         }
     }
 }
