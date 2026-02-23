7f97de0609f6c5662ed82e865e2cfde6b4b3cabd
JAVA_Tutorial <ahmad2020jafari@gmail.com>
2026-02-23 18:05:13 +0100

    Make the code reusable and create a reusable method

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,15 @@ public class Main {
             finalScore += (levelCompleted * bonus);
             finalScore += 1000;
             System.out.println("Your final score was : " + finalScore);
+           System.out.println( "The next high Score is: " + calclateScore(gameOver, score, levelCompleted, bonus));
         }
+    }
+    public static int calclateScore(boolean gameOver, int score, int levelCompleted, int bonus){
+      int finalScore = score;
+      if (gameOver){
+          finalScore += (levelCompleted * bonus);
+          finalScore +=1000;
+      }
+      return finalScore;
     }
 }
